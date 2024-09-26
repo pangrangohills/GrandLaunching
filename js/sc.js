b27e4f050be90fa55853ec4b9ba828a15f5c3073
EezzuuTT <rezastein531@gmail.com>
2024-09-26 16:33:41 +0700

    revisi

--- js/sc.js
+++ js/sc.js
@@ -1,23 +1,9 @@
-let imgBox = document.getElementById("imgBox");
-let qrImg = document.getElementById("qrImg");
-
-function genQr(inputText) {
-  qrImg.src =
-    "https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=" +
-    inputText;
-}
-
 document.getElementById("pushdata").onclick = function (e) {
   let name = document.getElementById("names").value;
   let domi = document.getElementById("domi").value;
   let notelp = document.getElementById("numbers").value;
   let agent = document.getElementById("agent").value;
-
-  var qr = "TPH-" + name + "-" + agent;
-
-  for (let i = 0; i < 6; i++) {
-    genQr(qr);
-  }
+  let qrImg = document.getElementById("qrImg");
 
   let datas = `Nama : ${name} Domisili : ${domi} No Telpon : ${notelp} Agent/Marketing : ${agent}`;
   var token = "7655916759:AAFN33QwRoKdN4XccJ41bvnGttIpyBVVJkI";
@@ -27,6 +13,5 @@ document.getElementById("pushdata").onclick = function (e) {
   let api = new XMLHttpRequest();
   api.open("GET", url, true);
   api.send();
-  alert("Success! Silahkan Tekan & Tahan QR code Untuk mendownload!");
   alert("Success! Registration !");
 };
